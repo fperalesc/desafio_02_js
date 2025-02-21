987f61ae0643307dd7c79d9caeffa249eb9e1453
Felipe_Perales <f.perales.c@gmail.com>
2025-02-21 11:53:46 -0300

    fix img fondoCaja

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,15 @@ function bordeRojo() {
   let borde = document.getElementById("caja");
   let fondo = document.getElementById("contenedor-problema1");
   let h2 = document.getElementById("h2-Caja");
-  if (borde.style.border == "" ) {
+  if (borde.style.border == "") {
     borde.style.border = "2px solid red";
-    fondo.style.backgroundImage = 'url(/assets/imgs/fondo-caja.jpg)';
+    fondo.style.backgroundImage = "url(/assets/imgs/fondoCaja.jpg)";
     fondo.style.backgroundSize = "cover";
     fondo.style.backgroundPositionY = "50%";
     h2.style.color = "rgb(255, 0, 0)";
     h2.style.opacity = "0.9";
     h2.style.textShadow = "2px 2px 2px rgb(133, 18, 16)";
-    borde.style.animations
+    borde.style.animations;
   } else {
     borde.style.border = "";
     fondo.style.backgroundImage = "";
@@ -23,28 +23,32 @@ function bordeRojo() {
 
 // PROBLEMA #2
 function verificacion() {
-    let sticker1 = parseInt(document.getElementById("sticker1").value) || 0;
-    let sticker2 = parseInt(document.getElementById("sticker2").value) || 0;
-    let sticker3 = parseInt(document.getElementById("sticker3").value) || 0;
-    let mensaje = document.getElementById("mensaje");
+  let sticker1 = parseInt(document.getElementById("sticker1").value) || 0;
+  let sticker2 = parseInt(document.getElementById("sticker2").value) || 0;
+  let sticker3 = parseInt(document.getElementById("sticker3").value) || 0;
+  let mensaje = document.getElementById("mensaje");
 
-    console.log(mensaje)
-
-    if (sticker1 + sticker2 + sticker3 <= 10 && sticker1 + sticker2 + sticker3 >= 0 && sticker1 + sticker2 + sticker3 != 1) {
-      mensaje.innerText = `Llevas ${sticker1 + sticker2 + sticker3} stickers`;
-      mensaje.style.color = "black";
-    } else if (sticker1 + sticker2 + sticker3 > 10){
-        mensaje.innerText = `¡Llevas demasiados stickers (${sticker1+sticker2+sticker3})! Puedes llevar un máximo de 10 unidades`;
-        mensaje.style.color = "red"
-    } else if (sticker1 + sticker2 + sticker3 == 1) {
-      mensaje.innerText = `Llevas ${sticker1 + sticker2 + sticker3} sticker`;
-      mensaje.style.color = "black";
-    } else {
-        mensaje.innerText = "No puedes seleccionar números negativos. ¡Inténtalo otra vez!";
-        mensaje.style.color = "red";
-    }
+  if (
+    sticker1 + sticker2 + sticker3 <= 10 &&
+    sticker1 + sticker2 + sticker3 >= 0 &&
+    sticker1 + sticker2 + sticker3 != 1
+  ) {
+    mensaje.innerText = `Llevas ${sticker1 + sticker2 + sticker3} stickers`;
+    mensaje.style.color = "black";
+  } else if (sticker1 + sticker2 + sticker3 > 10) {
+    mensaje.innerText = `¡Llevas demasiados stickers (${
+      sticker1 + sticker2 + sticker3
+    })! Puedes llevar un máximo de 10 unidades`;
+    mensaje.style.color = "red";
+  } else if (sticker1 + sticker2 + sticker3 == 1) {
+    mensaje.innerText = `Llevas ${sticker1 + sticker2 + sticker3} sticker`;
+    mensaje.style.color = "black";
+  } else {
+    mensaje.innerText =
+      "No puedes seleccionar números negativos. ¡Inténtalo otra vez!";
+    mensaje.style.color = "red";
+  }
 }
-
 
 // PROBLEMA #3
 
